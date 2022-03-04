19fdf45663c198643312c6fb9d146854570412f1
JeongbinPark <qkrwjdqls99@gmail.com>
2022-03-04 23:38:16 +0900

    fix(auth middleware): fix valid user token

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -6,7 +6,7 @@ let auth = (req, res, next) => {
   //현재 쿠키에 저장된 토큰으로부터 사용자 _id를 찾아내고 전달.
   User.findByToken(token, (err, user)=>{
     if(err) throw err;
-    if(!user.length) return res.json({ isAuth: false, error: true})
+    if(!user) return res.json({ isAuth: false, error: true})
 
     //여기에서 req에 token과 user를 넣어주면, 다음 단계에 req 정보에서 가져다 쓸 수 있다.
     req.token = token;
